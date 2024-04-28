27cc1441fcefabf53fa137aaf049171fcc3b5e91
shiva kumar kella <shivakumar.kella@gmail.com>
2024-04-28 11:52:13 +0530

    first commit

---
+++ namasteReact/App.js
@@ -0,0 +1,7 @@
+const header = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World , jai Shree Ram"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
