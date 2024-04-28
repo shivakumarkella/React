138c7dfd8c4816e5411eb162661c511bf5a94b3e
shiva kumar kella <shivakumar.kella@gmail.com>
2024-04-28 21:02:16 +0530

    secondCommit

--- namasteReact/App.js
+++ namasteReact/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const header = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World , jai Shree Ram"
 );
+
+const Title = () => {
+  return <h1>This is the title of the page, nested Component</h1>;
+};
+//create react-functional element
+const HeaderJsx = () => (
+  <div>
+    <Title />
+    <h1>Om Namha Shivayaaa . . functional</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(<HeaderJsx />);
